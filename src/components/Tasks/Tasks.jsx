94327b797f26af5888b68b995008be9381c47895
Rash6 <97577733+Rash6@users.noreply.github.com>
2022-06-08 20:47:35 +0530

    d

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./tasks.module.css";
-
+import {Counter} from "../Counter"
 const Tasks = () => {
   // NOTE: do not delete `data-testid` key value pair
   return (
@@ -9,7 +9,10 @@ const Tasks = () => {
         {/* Task List */}
       </ul>
       <div data-testid="tasks-empty" className={styles.empty}>
+       
         {/* Show when No Tasks are present */}
+        <Counter/>
+
       </div>
     </>
   );
